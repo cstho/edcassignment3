661d6d4a478a80dc266284c3a6eac51c352e6ba7
Chris <hochris338@gmail.com>
2022-11-06 16:12:13 +1030

    GUI Updates (GridLayout)
    Updated the GUI so that it was more seperated

--- GpsGUI.java
+++ GpsGUI.java
@@ -1,5 +1,7 @@
 import nz.sodium.*;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
 import java.awt.*;
 import java.awt.FlowLayout;
 import java.text.SimpleDateFormat;  
@@ -9,15 +11,39 @@ import swidgets.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/** 
- * An example of how to use the GpsService class
- */
+
 public class GpsGUI {
 
     public static void main(String[] args){
+        /*JFrame tweaking */
 
         JFrame frame = new JFrame("GPS GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel gui = new JPanel(new BorderLayout(5,5));
+        gui.setBorder( new TitledBorder("GPS Management") );
+
+        //JToolBar tb = new JToolBar();
+        final JPanel panel = new JPanel(new GridLayout(5,5,3,3));
+        panel.setBorder(
+            new TitledBorder("All Trackers") );
+        gui.add(panel, BorderLayout.NORTH);
+
+        final JPanel panel2 = new JPanel(new FlowLayout());
+        panel2.setBorder(
+            new TitledBorder("Last Tracker Updated") );
+        gui.add(panel, BorderLayout.NORTH);
+        gui.add(panel2);
+
+        
+        frame.setContentPane(gui);
+
+        frame.pack();
+
+        frame.setSize(800, 300); 
+        frame.setVisible(true);
+
+
 
         /*--------------------------------- */
         /* Trackers (part 1) */
@@ -25,60 +51,60 @@ public class GpsGUI {
         STextField buffer0 = new STextField("");
         Cell<String> text0 = input0.sUserChanges.hold(buffer0.getText()); //Hold buffer value so value doesnt disappear
         SLabel label0 = new SLabel(text0);
-        frame.add(label0);
+        panel.add(label0);
         STextField input1 = new STextField("");
         STextField buffer1 = new STextField("");
         Cell<String> text1 = input1.sUserChanges.hold(buffer1.getText()); //Hold buffer value so value doesnt disappear
         SLabel label1 = new SLabel(text1);
-        frame.add(label1);
+        panel.add(label1);
 
         STextField input2 = new STextField("");
         STextField buffer2 = new STextField("");
         Cell<String> text2 = input2.sUserChanges.hold(buffer2.getText()); //Hold buffer value so value doesnt disappear
         SLabel label2 = new SLabel(text2);
-        frame.add(label2);
+        panel.add(label2);
 
         STextField input3 = new STextField("");
         STextField buffer3 = new STextField("");
         Cell<String> text3 = input3.sUserChanges.hold(buffer3 .getText()); //Hold buffer value so value doesnt disappear
         SLabel label3 = new SLabel(text3);
-        frame.add(label3);
+        panel.add(label3);
 
         STextField input4 = new STextField("");
         STextField buffer4 = new STextField("");
         Cell<String> text4 = input4.sUserChanges.hold(buffer4.getText()); //Hold buffer value so value doesnt disappear
         SLabel label4 = new SLabel(text4);
-        frame.add(label4);
+        panel.add(label4);
 
         STextField input5 = new STextField("");
         STextField buffer5 = new STextField("");
         Cell<String> text5 = input5.sUserChanges.hold(buffer5.getText()); //Hold buffer value so value doesnt disappear
         SLabel label5 = new SLabel(text5);
-        frame.add(label5);
+        panel.add(label5);
 
         STextField input6 = new STextField("");
         STextField buffer6 = new STextField("");
         Cell<String> text6 = input6.sUserChanges.hold(buffer6.getText()); //Hold buffer value so value doesnt disappear
         SLabel label6 = new SLabel(text6);
-        frame.add(label6);
+        panel.add(label6);
 
         STextField input7 = new STextField("");
         STextField buffer7 = new STextField("");
         Cell<String> text7 = input7.sUserChanges.hold(buffer7.getText()); //Hold buffer value so value doesnt disappear
         SLabel label7 = new SLabel(text7);
-        frame.add(label7);
+        panel.add(label7);
 
         STextField input8 = new STextField("");
         STextField buffer8 = new STextField("");
         Cell<String> text8 = input8.sUserChanges.hold(buffer8.getText()); //Hold buffer value so value doesnt disappear
         SLabel label8 = new SLabel(text8);
-        frame.add(label8);
+        panel.add(label8);
 
         STextField input9 = new STextField("");
         STextField buffer9 = new STextField("");
         Cell<String> text9 = input9.sUserChanges.hold(buffer9.getText()); //Hold buffer value so value doesnt disappear
         SLabel label9 = new SLabel(text9);
-        frame.add(label9);
+        panel.add(label9);
 
         /*--------------------------------- */
 
@@ -87,7 +113,8 @@ public class GpsGUI {
         STextField bufferTimestamp = new STextField("");
         Cell<String> textTimestamp = inputTimestamp.sUserChanges.hold(bufferTimestamp.getText());
         SLabel labelTimestamp = new SLabel(textTimestamp);
-        frame.add(labelTimestamp);
+        labelTimestamp.setForeground(Color.RED);
+        panel2.add(labelTimestamp);
 
         /*--------------------------------- */
         /* Timer handling - clear after 3 seconds of nothing */
@@ -279,10 +306,6 @@ public class GpsGUI {
         } 
 
 
-
-        frame.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 25));    
-        frame.setSize(400, 560);
-        frame.setVisible(true);
     }
 
 } 
